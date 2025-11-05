3fc7b3156e1f661a1d387b983a0d0ca14447b80b
Inventor Andy <inventorandy@hansodynamic.com>
2025-11-05 18:14:51 +0000

    explicitly set docker hub username for build

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker build -t us-central1-docker.pkg.dev/pyrokv/pyrokv/pyrokv-server:${IMAGE_TAG} \
-              -t us-central1-docker.pkg.dev/pyrokv/pyrokv/pyrokv-server:${IMAGE_SHA} \
-              -t us-central1-docker.pkg.dev/pyrokv/pyrokv/pyrokv-server:latest \
-              -t pyrokv:${IMAGE_TAG} \
-              -t pyrokv:${IMAGE_SHA} \
-              -t pyrokv:latest \
-              -f crates/pyrokv-server/Dockerfile .
